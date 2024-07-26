2c7f54e54fc0854951eb0c3070cd90a5e62818cf
Jouni Alanen <jouni@diory.me>
2024-07-26 12:16:18 +0300

    Add support for REDIS_URL

--- src/redisClientFactory.ts
+++ src/redisClientFactory.ts
@@ -1,10 +1,11 @@
 import { Redis } from 'ioredis';
 
 export const redisClientFactory = (): Redis => {
-  const redisClient = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-  });
+  const host = process.env.REDIS_HOST || 'localhost';
+  const port = parseInt(process.env.REDIS_PORT, 10) || 6379;
+  const redisUrl = process.env.REDIS_URL ?? `redis://${host}:${port}`;
+
+  const redisClient = new Redis(redisUrl);
 
   redisClient.on('error', (e) => {
     throw new Error(`Redis client failed to connect: ${e}`);
