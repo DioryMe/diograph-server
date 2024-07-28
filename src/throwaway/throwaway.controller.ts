2401e71661f8e2324efb9881e116e7745d9fc918
Jouni Alanen <jouni@diory.me>
2024-07-28 22:34:52 +0300

    Retrieve AWS accessToken and secret with ID token + make S3 request to verify them

--- src/throwaway/throwaway.controller.ts
+++ src/throwaway/throwaway.controller.ts
@@ -9,20 +9,81 @@ import {
 } from '@nestjs/common';
 import { Redis } from 'ioredis';
 
+import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+import {
+  CognitoIdentityClient,
+  GetIdCommand,
+  GetCredentialsForIdentityCommand,
+} from '@aws-sdk/client-cognito-identity';
+
+const getCredentials = async (accessToken, identityToken) => {
+  const cognitoIdentityClient = new CognitoIdentityClient({
+    region: 'eu-north-1',
+  });
+
+  // Get the identity ID
+  const getIdCommand = new GetIdCommand({
+    IdentityPoolId: 'eu-north-1:163257d1-b8f4-4fbd-88e1-7edf6d07d7d7',
+    Logins: {
+      'cognito-idp.eu-north-1.amazonaws.com/eu-north-1_Vvg0QXGcW':
+        identityToken,
+    },
+  });
+
+  const identityResponse = await cognitoIdentityClient.send(getIdCommand);
+  const identityId = identityResponse.IdentityId;
+
+  // Get the credentials for the identity
+  const getCredentialsCommand = new GetCredentialsForIdentityCommand({
+    IdentityId: identityId,
+    Logins: {
+      'cognito-idp.eu-north-1.amazonaws.com/eu-north-1_Vvg0QXGcW':
+        identityToken,
+    },
+  });
+
+  const credentialsResponse = await cognitoIdentityClient.send(
+    getCredentialsCommand,
+  );
+  const credentials = credentialsResponse.Credentials;
+
+  return credentials;
+};
+
 @Controller('throwaway')
 export class ThrowawayController {
   constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
   @Get('callback')
-  @Redirect('http://localhost:5173')
+  // @Redirect('http://localhost:5173')
   async callbackAction(
     @Query() query: Record<string, string>,
-    @Session() session: Record<string, string>,
+    // @Session() session: Record<string, any>,
   ) {
     const date = new Date().toISOString();
     await this.redisClient.set('date', date);
 
-    session.token = query.token;
+    const credentials = await getCredentials(query.token, query.id_token);
+
+    const options = {
+      region: 'eu-west-1',
+      credentials: {
+        accessKeyId: credentials.AccessKeyId,
+        secretAccessKey: credentials.SecretKey,
+        sessionToken: credentials.SessionToken,
+      },
+    };
+
+    const s3Client = new S3Client(options);
+
+    const listCommand = new ListObjectsV2Command({
+      Bucket: 'jvalanen-diory-test3',
+      Prefix: 'room/',
+    });
+
+    const list = await s3Client.send(listCommand);
+
+    return 'jee' + JSON.stringify(list);
   }
 
   @Get('test')
